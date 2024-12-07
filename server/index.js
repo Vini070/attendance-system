bc2da47caacfe9b54b0e2b2b29d2ae3ea952dc9d
Vinitha Kengatharan <87264797+Vini070@users.noreply.github.com>
2024-12-07 14:59:57 +0530

    #3, implemented login API with JWT suppor

--- server/index.js
+++ server/index.js
@@ -9,3 +9,21 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = "secret123";
+
+const users = [{ username: "admin", password: "password" }];
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+        res.json({ token });
+    } else {
+        res.status(401).json({ message: "Invalid username or password" });
+    }
+});
+
